d0e8c3bcf2724422608215aa3a6cd0392e25d98f
MANASA <Manasa.hk6@gmail.com>
2017-06-12 22:54:11 +0530

    small update

---
+++ test/src/test/java/pac1/Class2.java
@@ -0,0 +1,16 @@
+package pac1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Class2 {
+	WebDriver driver;
+	@Test
+	public void firsdrivt()
+	{
+System.setProperty("webdriver.gecko.driver", "Drivers/geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+	}
+}
